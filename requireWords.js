bb004af9a199e482523abf5f30e1c9511f9b5e37
gabriel.mfcorreia <gabriel.mfcorreia@gmail.com>
2016-04-13 11:55:08 -0400

    v2.1

--- requireWords.js
+++ requireWords.js
@@ -2,7 +2,7 @@ var request = require("request");
 var Foswig = require('foswig');
 var clc = require('cli-color');
 
-var url = "https://pt.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&format=json";
+var url = "https://sv.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&format=json";
 
 function getParagraph(ret) {
   request({
@@ -21,6 +21,7 @@ function getDicionary(callback) {
     var dicio = [];
     for (var i in paragraph.query.pages) {
       var text = paragraph.query.pages[i].extract;
+      var title = paragraph.query.pages[i].title;
 
       // HTML
       text = text.replace(/<(?:.|\n)*?>/g, '');
@@ -32,7 +33,7 @@ function getDicionary(callback) {
       var palavras = [];
 
       palavras = text.split(" ");
-
+      dicio.push(title);
       for (var ii = 0; ii < palavras.length; ii++) {
 
         if (palavras[ii].length > 1) {
@@ -60,8 +61,10 @@ function imprimeDicio() {
 function getValidDicio(callback) {
   getDicionary(function(dicio) {
     if (dicio.length > 500) {
+      console.log(clc.green("Artigo aceito " + dicio.length + " Titulo: " + dicio[0]));
       callback(dicio);
     } else {
+      console.log(clc.red("Artigo rejeitado " + dicio.length));
       getValidDicio(callback);
     }
   });
@@ -70,7 +73,8 @@ function getValidDicio(callback) {
 function getWords(quant) {
 
   getValidDicio(function(dicio) {
-    var chain = new Foswig(5);
+    var chain = new Foswig(3);
+    dicio.splice(0, 1);
     chain.addWordsToChain(dicio);
     for (var cont = 0; cont < quant; cont++) {
       var pal = chain.generateWord(2, 15, false, 300);
